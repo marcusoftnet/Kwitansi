8c7233f90741ef87968c6793db35c2518f12531e
Marcus Hammarberg <marcusoft.net@gmail.com>
2014-06-11 12:02:31 +0700

    Got most of it to work

--- app.js
+++ app.js
@@ -2,19 +2,57 @@ var koa = require("koa");
 var app = module.exports = koa();
 var config = require('./config')();
 var routes = require("koa-route");
+var parse = require("co-body");
 var serve = require('koa-static');
+var render = require("./lib/render.js");
+var dbWrap = require("./lib/dbWrap.js");
+var hospitalConfigs = dbWrap.getCollection(config.mongoUrl, "hospitalConfig");
+var kwitansis = dbWrap.getCollection(config.mongoUrl, "kwitansi");
 
 // middleware
 app.use(serve(__dirname + '/public'));
 
 // routes
-// chunks admin
-
-// api routes
-app.use(function *() {
-	this.body = "hello";
-});
+app.use(routes.get("/:hospital", create));
+app.use(routes.post("/:hospital", print));
 
 // fire it up
 app.listen(config.port);
 console.log('The app is listening. Port:'+ config.port);
+
+// handlers
+function *create(hospital) {
+	var vm = yield hospitalConfigs.findOne({name: hospital});
+
+	// Create current view model
+	// var highestKwitansiNo = yield kwitansis.find()
+	vm.nextKwitansiNo = vm.latestsKwitansiNo + 1;
+	vm.kwitansiDate = new Date().toISOString().slice(0,10);
+
+	this.body = yield render("create.html", vm);
+};
+
+function *print(hospital) {
+	var postedData = yield parse(this);
+
+	// Save to database
+	yield kwitansis.insert(postedData);
+
+	// Create viewmodel
+	var vm = postedData;
+	vm.hospitalName = hospital;
+
+	this.body = yield render("print.html", vm);
+};
+
+
+
+
+
+
+
+
+
+
+
+
--- test/testHelpers.js
+++ test/testHelpers.js
@@ -1,11 +1,15 @@
 var co = require("co");
 var config = require("../config")("local");
-var dbWrap = require("../lib/dbWrap.js");
+var dbWrap = require("./lib/dbWrap.js");
 var hospitalConfigs = dbWrap.getCollection(config.mongoUrl, "hospitalConfig");
+var kwitansis = dbWrap.getCollection(config.mongoUrl, "kwitansi");
+
+module.exports.kwitansis = kwitansis;
 module.exports.hospitalConfigs = hospitalConfigs;
 
 module.exports.removeAllDocs = function(done){
 	co(function *(){
+		yield kwitansis.remove({});
 		yield hospitalConfigs.remove({});
 	})(done);
 };
